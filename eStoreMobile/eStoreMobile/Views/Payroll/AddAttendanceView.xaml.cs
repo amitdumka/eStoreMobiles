57646c1d1e78af1ce6e9dc42d06ad98689ac752c
Amit Kumar <amitnarayansah@gmail.com>
2021-10-03 03:36:49 +0530

    Form changed

--- eStoreMobile/eStoreMobile/Views/Payroll/AddAttendanceView.xaml.cs
+++ eStoreMobile/eStoreMobile/Views/Payroll/AddAttendanceView.xaml.cs
@@ -11,26 +11,67 @@ namespace eStoreMobile.Views
         {
             InitializeComponent ();
         }
-        private async void AddButton_Clicked(object sender, EventArgs e)
+        private void dataForm_AutoGeneratingDataFormItem(object sender, Syncfusion.XForms.DataForm.AutoGeneratingDataFormItemEventArgs e)
         {
-            if ( await DisplayAlert ("Alert", "Do you want to save!", "Yes", "No") )
-            {
-                //Go Back
-            }
+            // Attendance at; at.
+
+            if (e.DataFormItem.Name == "IsTailoring")
+                e.DataFormItem.Editor = "Switch";
+            if (e.DataFormItem.Name == "IsReadOnly")
+                e.Cancel = true;
+            if (e.DataFormItem.Name == "AttendanceId")
+                e.Cancel = true;
+            if (e.DataFormItem.Name == "EmployeeId")
+                e.Cancel = true;
+            if (e.DataFormItem.Name == "Employee")
+                e.Cancel = true;
+            if (e.DataFormItem.Name == "Store")
+                e.Cancel = true;
+            if (e.DataFormItem.Name == "StoreId")
+                e.Cancel = true;
+
+
+            if (e.DataFormItem.Name == "UserId")
+                e.Cancel = true;
+
+
+
+        }
+        //private async void AddButton_Clicked(object sender, EventArgs e)
+        //{
+        //    if ( await DisplayAlert ("Alert", "Do you want to save!", "Yes", "No") )
+        //    {
+        //        //Go Back
+        //    }
+        //}
+
+        //private async void CancelButton_Clicked(object sender, EventArgs e)
+        //{
+        //    if ( await DisplayAlert ("Alert", "Are your sure to go back!", "Yes", "No") )
+        //    {
+        //        //Go Back
+        //    }
+
+        //}
+
+        //private void isTailoringSwitch_StateChanged(object sender, Syncfusion.XForms.Buttons.SwitchStateChangedEventArgs e)
+        //{
+
+        //}
+    }
+    public class AttendanceViewModel
+    {
+        private Attendance attendance { get; set; }
+
+        public Attendance Attendance
+        {
+            get { return this.attendance; }
+            set { this.attendance = value; }
         }
 
-        private async void CancelButton_Clicked(object sender, EventArgs e)
+        public AttendanceViewModel()
         {
-            if ( await DisplayAlert ("Alert", "Are your sure to go back!", "Yes", "No") )
-            {
-                //Go Back
-            }
-
-        }
-
-        private void isTailoringSwitch_StateChanged(object sender, Syncfusion.XForms.Buttons.SwitchStateChangedEventArgs e)
-        {
-
+            this.attendance = new Attendance();
         }
     }
 }
