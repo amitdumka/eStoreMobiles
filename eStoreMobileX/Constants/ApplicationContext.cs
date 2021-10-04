94f26c9b5f5d64c946700470813a1d2458dcaa93
Amit Kumar <amitnarayansah@gmail.com>
2021-10-04 10:38:09 +0530

    Removing old obs
    ds

---
+++ eStoreMobileX/Constants/ApplicationContext.cs
@@ -0,0 +1,15 @@
+namespace eStoreMobileX{
+
+
+public static class ApplicationContext
+    {
+        public static string UserName { get; set; }
+        public static EmpType Role { get; set; }
+        public static bool IsLoggedIn { get; set; }
+        
+        public static int EmpId { get; set; }
+        public static int StoreId { get; set; }
+        public static string StoreName { get; set; }
+
+    }
+}
