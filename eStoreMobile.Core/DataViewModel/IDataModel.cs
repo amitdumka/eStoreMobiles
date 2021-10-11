d6ddb8796c8f632fd41c7a91321e255e23db03ae
Amit Kumar <amitnarayansah@gmail.com>
2021-10-11 08:29:51 +0530

    party and abstract data model is added.

---
+++ eStoreMobile.Core/DataViewModel/IDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eStoreMobile.Core.DataViewModel
+{
+    public interface IDataModel<T>
+    {
+        public Task<List<T>> GetItems(int storeid, bool local = false);
+        public Task<bool> Sync();
+        public Task<T> GetById(int id);
+        public Task<bool> Save(T item, bool isNew = true, bool local = false);
+        public Task<bool> Delete(int id);
+        public Task<bool> IsExists(int id);
+    }
+}
--- eStoreMobile.Core/DataViewModel/PaymentDataModel.cs
+++ eStoreMobile.Core/DataViewModel/PaymentDataModel.cs
@@ -23,6 +23,7 @@ namespace eStoreMobile.Core.DataViewModel
         {
             return (await service.RefreshDataAsync()).Where(c=>c.StoreId==storeid).ToList();
         }
+
         public async Task<List<Payment>> FindAsync(int storeid, string query,bool local = false)
         {
             return (await service.FindAsync(query)).Where(c => c.StoreId == storeid).ToList();
@@ -46,15 +47,7 @@ namespace eStoreMobile.Core.DataViewModel
         
     }
 
-    public interface IDataModel<T>
-    {
-        public Task<List<T>> GetItems(int storeid, bool local = false);
-        public Task<bool> Sync();
-        public Task<T> GetById(int id);
-        public Task<bool>  Save(T item, bool isNew = true, bool local = false);
-        public Task<bool> Delete(int id);
-        public Task<bool> IsExists(int id);    
-    }
+    
     public class CashPaymentDataModel
     {
         public List<CashPayment> Payments { get; set; }
