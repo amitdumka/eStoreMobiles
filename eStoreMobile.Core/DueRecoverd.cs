8aeb66e47e2e8f1da975aeaed61e990e7205b9a7
Amit Kumar <amitnarayansah@gmail.com>
2021-10-13 08:00:04 +0530

    some changes

---
+++ eStoreMobile.Core/DueRecoverd.cs
@@ -0,0 +1,37 @@
+﻿using eStore.Shared.Models.Stores;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eStore.Shared.Models.Sales
+{
+    public class DueRecoverd
+    {
+        public int DueRecoverdId { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Recovery Date")]
+        public DateTime PaidDate { get; set; }
+
+        public int DuesListId { get; set; }
+        public virtual DuesList DuesList { get; set; }
+
+        [DataType(DataType.Currency), Column(TypeName = "money")]
+        public decimal AmountPaid { get; set; }
+
+        [Display(Name = "Is Partial Payment")]
+        public bool IsPartialPayment { get; set; }
+
+        public PaymentMode Modes { get; set; }
+        public string Remarks { get; set; }
+
+        //Version 3.0
+        [DefaultValue(1)]
+        public int StoreId { get; set; }
+
+        public virtual Store Store { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ eStoreMobile.Core/DuesList.cs
@@ -0,0 +1,34 @@
+﻿using eStore.Shared.Models.Stores;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eStore.Shared.Models.Sales
+{
+    public class DuesList
+    {
+        public int DuesListId { get; set; }
+
+        [DataType(DataType.Currency), Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [Display(Name = "Is Paid")]
+        public bool IsRecovered { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Recovery Date")]
+        public DateTime? RecoveryDate { get; set; }
+
+        public int DailySaleId { get; set; }
+        public virtual DailySale DailySale { get; set; }
+        public bool IsPartialRecovery { get; set; }
+
+        //Version 3.0
+        [DefaultValue(1)]
+        public int StoreId { get; set; }
+
+        public virtual Store Store { get; set; }
+        public string UserId { get; set; }
+    }
+}
